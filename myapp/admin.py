c14b6930b995aed9fe8f852d91fe279a07b40fe8
devsubang <devsubang@gmail.com>
2018-09-03 14:08:02 +0700

    Hai

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Course,Lesson
+from .models import Course
 
 admin.site.register(Course)
-admin.site.register(Lesson)
-# Register your models here.
