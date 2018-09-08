87ec12f7b7e42810e80c7f37da7466db395bb06e
devsubang <devsubang@gmail.com>
2018-09-09 01:36:57 +0700

    NIh

--- myapp/models.py
+++ myapp/models.py
@@ -24,6 +24,17 @@ class Lesson(models.Model):
     def  __str__(self):
         return '[%s] %s' % (self.code_name, self.order)
 
+class Learn(models.Model):
+    order = models.IntegerField()
+    code_name = models.CharField(max_length=10)
+    materi = models.CharField(max_length=20)
+    name = models.CharField(max_length=70, default='')
+    img = models.ImageField(blank=True, upload_to='static/img')
+    content = RichTextUploadingField()
+
+    def  __str__(self):
+        return '[%s] %s' % (self.code_name, self.order)
+
 class Event(models.Model):
     name = models.CharField(max_length=70, default='')
     lokasi = models.CharField(max_length=50)
