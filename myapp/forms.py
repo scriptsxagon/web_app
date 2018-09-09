9ddd768a009442575d723f2cf5c6fe72d00e2c79
devsubang <devsubang@gmail.com>
2018-09-10 01:53:08 +0700

    nih

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Masukan email dengan benar.')
-    name =  forms.CharField(max_length=50, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'name', 'password1', 'password2', )
+        fields = ('username', 'email', 'first_name','last_name', 'password1', 'password2', )
